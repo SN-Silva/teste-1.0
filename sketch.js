9f6a7d468da2d0404cf3ab7396d8c1cc1948f774
SN-Silva <sn.borges.silva@gmail.com>
2026-05-13 22:36:50 -0300

    acrescentei o fundo e um obstaculo.

--- sketch.js
+++ sketch.js
@@ -1,10 +1,14 @@
-var campox = 500, campoy = 300;
+var mapaimg;
+var predioimg;
+var campox = 5000, campoy = 300;
 var patinhoimg;
 var patinhoX = 0, patinhoY = 100;
 var patinhoSpeed = 1;
 
 function preload() {
-  patinhoimg = loadImage('patinho.png'); // Load any assets here if needed
+  patinhoimg = loadImage('patinho.png'); // Carrega a imagem do patinho
+  mapaimg = loadImage('mapa.avif');  // Carrega a imagem do mapa
+  predioimg = loadImage('predio.png');  // Carrega a imagem do prédio
 }
 
 function setup() {
@@ -13,8 +17,22 @@ function setup() {
 
 function draw() {
   background(0, 0, 0);
+  mapa();
   patinho();
+  predio();
 }
+
+function mapa() {
+  image(mapaimg, 0, 0, campox, campoy);
+}
+
+function predio() {
+  image(predioimg, 300, 120, 100, 200);
+}
+
+
+
+
 
 function patinho() {
   image(patinhoimg, patinhoX, patinhoY, 140, 50);
